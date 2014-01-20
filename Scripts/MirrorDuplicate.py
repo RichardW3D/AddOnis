e842692b561a136ce64903515fb07a2c1128092b
RichardW3D <sethrain@googlemail.com>
2014-01-20 13:41:14 +0000

    Updating to use new category tabs
    Mirror Duplicate can now be found only in the 'Basic' category tab,
    along with regular mirror.

--- Scripts/MirrorDuplicate.py
+++ Scripts/MirrorDuplicate.py
@@ -1,26 +1,26 @@
 ##############################################################################
-#    Copyright © 2013  Richard Wilks                                         #
-#    August 11th                                                             #
+# Copyright © 2013 Richard Wilks                                             #
+# August 11th                                                                #
 #                                                                            #
-#    This program is free software: you can redistribute it and/or modify    #
-#    it under the terms of the GNU General Public License as published by    #
-#    the Free Software Foundation, either version 3 of the License, or       #
-#    (at your option) any later version.                                     #
+# This program is free software: you can redistribute it and/or modify       #
+# it under the terms of the GNU General Public License as published by       #
+# the Free Software Foundation, either version 3 of the License, or          #
+# (at your option) any later version.                                        #
 #                                                                            #
-#    This program is distributed in the hope that it will be useful,         #
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of          #
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
-#    GNU General Public License for more details.                            #
+# This program is distributed in the hope that it will be useful,            #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the               #
+# GNU General Public License for more details.                               #
 #                                                                            #
-#    A copy of the GPLv3 license is available at                             #
-#    https://www.gnu.org/licenses/gpl-3.0.html.                              #
+# A copy of the GPLv3 license is available at                                #
+# https://www.gnu.org/licenses/gpl-3.0.html.                                 #
 ##############################################################################
 
 bl_info = {
     "name": "Mirror Duplicate",
     "author": "Richard Wilks",
-    "version": (0, 0, 5),
-    "blender": (2, 68, 2),
+    "version": (0, 5, 1),
+    "blender": (2, 69, 9),
     "location": "View3D > Tool Shelf",
     "description": "Mirror objects around the 3D Cursor via duplication",
     "wiki_url": "https://github.com/RichardW3D/AddOnis/wiki/Mirror-Duplicate",
@@ -32,22 +32,22 @@ bpy.types.Scene.apply_mirror_duplicate = bpy.props.BoolProperty(
     name="Apply Mirror",
     description="Apply the scale and recalculate the normals",
     default=False)
-    
+
 bpy.types.Scene.linked_duplicate = bpy.props.BoolProperty(
     name="Linked",
     description="Create linked duplicates",
     default=False)
-    
+
 def restore_state():
-    scn = bpy.context.scene 
+    scn = bpy.context.scene
     selected = bpy.context.selected_objects
     apply_mirror = scn.apply_mirror_duplicate
-    
+
     #Restore locks and pivot to original states
     for obj in selected:
         scn.objects.active = obj
         obj.lock_scale = [False, False, False]
-        
+
         #Apply Scale and recalculate normals if Apply Mirror is checked
         if apply_mirror == True:
             bpy.ops.object.transform_apply(scale=True)
@@ -56,7 +56,7 @@ def restore_state():
             bpy.ops.mesh.normals_make_consistent()
             bpy.ops.mesh.select_all(action='DESELECT')
             bpy.ops.object.editmode_toggle()
-      
+
     scn.apply_mirror_duplicate = False
     scn.linked_duplicate = False
 
@@ -68,10 +68,10 @@ class Mirror_X(bpy.types.Operator):
     bl_context = "objectmode"
     bl_options = {"UNDO"}
 
-    def execute(self, context): 
+    def execute(self, context):
         bpy.ops.object.duplicate(linked=bpy.context.scene.linked_duplicate)
         selected = context.selected_objects
-        
+
         #Store current pivot point and switch to 3D Cursor
         pivot = bpy.context.space_data.pivot_point
         context.space_data.pivot_point = 'CURSOR'
@@ -79,19 +79,19 @@ class Mirror_X(bpy.types.Operator):
         for obj in selected:
             #Lock scaling in unwanted axes
             obj.lock_scale = [False, True, True]
-            
+
             #Correct the rotation of duplicate positive <--> negative
             obj.rotation_euler[1] = -obj.rotation_euler[1]
             obj.rotation_euler[2] = -obj.rotation_euler[2]
-                   
+
         bpy.ops.transform.resize(value=(-1, 1, 1))
-        
+
         #Restore settings to what they were before operator was called
         bpy.context.space_data.pivot_point = pivot
         restore_state()
         return {'FINISHED'}
 
-#Mirror along the Y-axis    
+#Mirror along the Y-axis
 class Mirror_Y(bpy.types.Operator):
     """Mirror objects across the Y-axis via duplication"""
     bl_idname = "object.mirror_y"
@@ -102,7 +102,7 @@ class Mirror_Y(bpy.types.Operator):
     def execute(self, context):
         bpy.ops.object.duplicate(linked=bpy.context.scene.linked_duplicate)
         selected = context.selected_objects
-        
+
         #Store current pivot point and switch to 3D Cursor
         pivot = bpy.context.space_data.pivot_point
         context.space_data.pivot_point = 'CURSOR'
@@ -110,18 +110,18 @@ class Mirror_Y(bpy.types.Operator):
         for obj in selected:
             #Lock scaling in unwanted axes
             obj.lock_scale = [True, False, True]
-            
+
             #Correct the rotation of duplicate positive <--> negative
             obj.rotation_euler[0] = -obj.rotation_euler[0]
             obj.rotation_euler[2] = -obj.rotation_euler[2]
-                 
+
         bpy.ops.transform.resize(value=(1, -1, 1))
-        
+
         #Restore settings to what they were before operator was called
         bpy.context.space_data.pivot_point = pivot
         restore_state()
         return {'FINISHED'}
-        
+
 class Mirror_Z(bpy.types.Operator):
     """Mirror objects across the Z-axis via duplication"""
     bl_idname = "object.mirror_z"
@@ -132,7 +132,7 @@ class Mirror_Z(bpy.types.Operator):
     def execute(self, context):
         bpy.ops.object.duplicate(linked=bpy.context.scene.linked_duplicate)
         selected = context.selected_objects
-        
+
         #Store current pivot point and switch to 3D Cursor
         pivot = bpy.context.space_data.pivot_point
         context.space_data.pivot_point = 'CURSOR'
@@ -140,13 +140,13 @@ class Mirror_Z(bpy.types.Operator):
         for obj in selected:
             #Lock scaling in unwanted axes
             obj.lock_scale = [True, True, False]
-            
+
             #Correct the rotation of duplicate positive <--> negative
             obj.rotation_euler[0] = -obj.rotation_euler[0]
             obj.rotation_euler[1] = -obj.rotation_euler[1]
-                             
+
         bpy.ops.transform.resize(value=(1, 1, -1))
-        
+
         #Restore settings to what they were before operator was called
         bpy.context.space_data.pivot_point = pivot
         restore_state()
@@ -157,18 +157,19 @@ class VIEW3D_PT_mirrorduplicate(bpy.types.Panel):
     bl_label = "Mirror Duplicate"
     bl_space_type = "VIEW_3D"
     bl_region_type = "TOOLS"
-    bl_context = "objectmode"  
+    bl_category = "Basic"
+    bl_context = "objectmode"
 
     def draw(self, context):
         layout = self.layout
         obj = context.object
         scn = context.scene
-        
+
         #If Linked is enabled, hide Apply Mirror and vice versa
         sub = layout.split()
         if scn.linked_duplicate == False:
             sub.prop(scn, 'apply_mirror_duplicate')
-        
+
         if scn.apply_mirror_duplicate == False:
             sub.prop(scn, 'linked_duplicate')
 
@@ -177,14 +178,14 @@ class VIEW3D_PT_mirrorduplicate(bpy.types.Panel):
         sub.operator('object.mirror_y')
         sub.operator('object.mirror_z')
 
-#registration of operator classes and panel class      
+#registration of operator classes and panel class
 def register():
     bpy.utils.register_class(VIEW3D_PT_mirrorduplicate)
     bpy.utils.register_class(Mirror_X)
     bpy.utils.register_class(Mirror_Y)
     bpy.utils.register_class(Mirror_Z)
 
-#unregistration of operator classes and panel class 
+#unregistration of operator classes and panel class
 def unregister():
     bpy.utils.unregister_class(VIEW3D_PT_mirrorduplicate)
     bpy.utils.unregister_class(Mirror_X)
